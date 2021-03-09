bd7111796a7f75e7d67b3cd12c981ce5003a3e03
Dayana Mick <dayana.mick@marleyspoon.com>
2021-03-09 09:21:48 +0100

    add sentry

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <form id="demo" @submit.prevent="processForm">
+    <!-- text -->
+    <label>Name:</label> <input type="text" v-model="name" />
+    <label>Price:</label> <input type="number" v-model="price" />
+    <!-- checkbox -->
+    <button>Submit</button>
+  </form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return { name: "", price: "", products: "" };
+  },
+  async mounted() {
+    // const response = await fetch(
+    //   "http://ec2-3-64-62-57.eu-central-1.compute.amazonaws.com:3000/products-list"
+    // );
+    // const data = await response.json();
+    // console.log(data, "response");
+    // this.products = data;
+  },
+  methods: {
+    processForm: async function() {
+      console.log({ name: this.name, product: this.product });
+      // const response = await fetch(
+      //   "http://ec2-3-64-62-57.eu-central-1.compute.amazonaws.com:3000/products",
+      //   {
+      //     method: "POST",
+      //     body: JSON.stringify({
+      //       name: this.name,
+      //       id: new Date(),
+      //       price: parseFloat(this.price),
+      //     }),
+      //   }
+      // );
+      // console.log({ response });
+    },
+  },
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import * as Sentry from "@sentry/vue";
+import { Integrations } from "@sentry/tracing";
 
-createApp(App).mount('#app')
+Sentry.init({
+  dsn:
+    "https://ad6312438f9444198af9275276c99e86@o545631.ingest.sentry.io/5667905",
+  integrations: [new Integrations.BrowserTracing()],
+  tracingOptions: {
+    trackComponents: true,
+  },
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
+
+createApp(App).mount("#app");
