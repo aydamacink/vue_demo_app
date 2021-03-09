407e3a596617451b63a2e2cca1fa15410ce6d285
Dayana Mick <dayana.mick@marleyspoon.com>
2021-03-09 09:31:21 +0100

    add compression

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  pluginOptions: {
+    compression: {
+      brotli: {
+        filename: "[path].br[query]",
+        algorithm: "brotliCompress",
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        compressionOptions: {
+          level: 11,
+        },
+        minRatio: 0.8,
+      },
+      gzip: {
+        filename: "[path].gz[query]",
+        algorithm: "gzip",
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        minRatio: 0.8,
+      },
+    },
+  },
+};
